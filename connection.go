e04f82d644d2070749d615c2f3db33f116ac22fd
Prashanth Chandra <coolshanth94@gmail.com>
2017-02-10 12:38:54 +0800

    Add known node hosting a post to DB

--- connection.go
+++ connection.go
@@ -26,3 +26,29 @@ func GetNodesHostingPost(postID string) ([]string, error) {
 	// @TODO check if postID is a valid hash
 	return knownNodes, nil
 }
+
+// AddNodeHostingPost adds the nodeID to the database for the given postID
+// @TODO Validate postID and nodeID
+func AddNodeHostingPost(postID string, nodeID string) error {
+	var knownNodes []string
+	err := db.Update(func(tx *bolt.Tx) error {
+		bucket := tx.Bucket([]byte("knownNodes"))
+		// If the postID already has an entry, unmarshal and append the nodeID to it
+		// else create a new array containing the nodeID
+		if data := bucket.Get([]byte(postID)); data != nil {
+			if err := json.Unmarshal(data, &knownNodes); err != nil {
+				return err
+			}
+			knownNodes = append(knownNodes, nodeID)
+		} else {
+			knownNodes = []string{nodeID}
+		}
+		data, _ := json.Marshal(knownNodes)
+		err := bucket.Put([]byte("knownNodes"), data)
+		return err
+	})
+	if err != nil {
+		fmt.Printf("Could not add Node ID %s to Post ID %s\n", nodeID, postID)
+	}
+	return err
+}
