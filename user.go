17f7996f4d1257f29431d4a7c86092d9ab413d15
Long Hoang <long@mindworker.de>
2017-03-07 14:41:30 +0800

    Generate user's private/public key and save user config on disc

--- user.go
+++ user.go
@@ -1,15 +1,89 @@
 package main
 
-// @TODO Use Constructor and Getter when interacting with User struct
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/x509"
+	"encoding/json"
+	"encoding/pem"
+	"io/ioutil"
+	"log"
+)
 
-var MyUser = &User{
-	PubKey:  "TODO",
-	PrivKey: "TODO",
-	Alias:   "Long",
+const (
+	// MyNodePath specifies the path where the node repository
+	// (containing data and configuration) of the default node
+	// used by the application is stored
+	MyUserConfPath = "./data/me.json"
+
+	// nBitsForKeypair sets the strength of keypair
+	nBitsForUserKeypair = 2048
+)
+
+var MyUser *User
+
+func init() {
+	var err error
+	if !Exists(MyUserConfPath) {
+		MyUser, err = NewUser("long")
+		if err != nil {
+			log.Fatal("Cannot generate new User identity", err)
+		}
+
+		userConfJSON, _ := json.Marshal(MyUser)
+		err = ioutil.WriteFile(MyUserConfPath, userConfJSON, 0644)
+
+		if err != nil {
+			log.Fatal("Cannot write new UserConf to disk", err)
+		}
+	} else {
+		file, err := ioutil.ReadFile(MyUserConfPath)
+		if err != nil {
+			log.Fatal("Cannot read user config", err)
+		}
+
+		var user User
+		json.Unmarshal(file, &user)
+		MyUser = &user
+	}
 }
 
 type User struct {
 	PubKey  string
 	PrivKey string
 	Alias   string
+}
+
+func NewUser(alias string) (*User, error) {
+	key, err := rsa.GenerateKey(rand.Reader, nBitsForUserKeypair)
+	if err != nil {
+		return nil, err
+	}
+
+	privKeyDer := x509.MarshalPKCS1PrivateKey(key)
+	privKeyBlock := pem.Block{
+		Type:    "RSA PRIVATE KEY",
+		Headers: nil,
+		Bytes:   privKeyDer,
+	}
+	privKeyPem := string(pem.EncodeToMemory(&privKeyBlock))
+
+	publicKey := key.PublicKey
+	publicKeyDer, err := x509.MarshalPKIXPublicKey(&publicKey)
+	if err != nil {
+		return nil, err
+	}
+
+	publicKeyBlock := pem.Block{
+		Type:    "PUBLIC KEY",
+		Headers: nil,
+		Bytes:   publicKeyDer,
+	}
+	publicKeyPem := string(pem.EncodeToMemory(&publicKeyBlock))
+
+	return &User{
+		PrivKey: privKeyPem,
+		PubKey:  publicKeyPem,
+		Alias:   alias,
+	}, nil
 }
